abfcabb8425df9ab42eeaacb207aa2da193b252a
jsilberkleit <jason.silberkleit@xpo.com>
2015-09-23 11:31:07 -0400

    added padleft and padright to string utils and updated gitignore to ignore webstorm stuff

---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = StringUtils;
+
+/* **
+    Library for manipulating strings
+    TODO: Make it manipulate the String prototype to add this with a safe method to just
+    return it as a seperate instance
+ */
+function StringUtils() {
+    if (!this instanceof StringUtils)
+        return new StringUtils();
+}
+
+/* **
+    Pads the left side of the string with value up to padding characters
+ */
+StringUtils.prototype = function padLeft(value, padding) {
+    padding = padding || 7;
+
+    value = String(value);
+
+    if (value.length > padding)
+        return value;
+
+    var padstr = "";
+    for (var i = 0; i < padding; i++)
+        padstr+= " ";
+
+    return (padstr + value).slice(-padstr.length);
+};
+
+/* **
+    Pads the right side of the string with value up to padding characters
+ */
+StringUtils.prototype = function padRight(value, padding) {
+    padding = padding || 7;
+
+    value = String(value);
+
+    if (value.length > padding)
+        return value;
+
+    var diff = padding - value.length;
+
+    var padstr = "";
+    for (var i = 0; i < diff; i++)
+        padstr+= " ";
+
+    return (value + padstr);
+};
